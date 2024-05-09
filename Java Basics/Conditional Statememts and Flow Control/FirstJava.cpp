c0ab927782dbc5f61505df301ae9fb3cc75e90fc
TheNaiveSamosa <khamitkaraditya@gmail.com>
2024-05-09 20:37:21 +0530

    Initial Commit

---
+++ Java Basics/Conditional Statememts and Flow Control/FirstJava.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is my first program" << std::endl;
+    return 0;
+}
--- Java Basics/Conditional Statememts and Flow Control/FirstJava.java
+++
@@ -1,6 +0,0 @@
-public class FirstJava {
-    public static void main(String args[]){
-        System.out.println("This is my first program")
-    }
-    
-}
